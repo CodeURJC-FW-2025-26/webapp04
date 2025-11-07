b7d32794cf9443950ffb47005f8bf319518882e5
Felix <felix.sch@protonmail.com>
2025-11-07 17:19:05 +0100

    Prepared Práctica 2: Added routes in `router.js`

--- src/router.js
+++ src/router.js
@@ -1,36 +1,101 @@
 import express from 'express';
+import { ObjectId } from 'mongodb';
+
+import * as movieCatalogue from './movieCatalogue.js';
+import * as actorCatalogue from './actorCatalogue.js';
 
 const router = express.Router();
 export default router;
 
-router.get('/', (req, res) => {
-
-    res.render('index', {
-
-    });
-
+router.get('/', async (req, res) => {
+    const movies = await movieCatalogue.getMovies();
+    res.render('index', { movies });
 });
 
-router.get('/movieDetails', (req, res) => {
 
-    res.render('movieDetails', {
+router.get('/movieDetails/:id', async (req, res) => {
+    try {
+        const movieId = new ObjectId(req.params.id);
+        const movie = await movieCatalogue.getMovie(movieId);
 
-    });
+        if (!movie) {
+            return res.status(404).send('Movie not found');
+        }
 
+        const actorsResolved = [];
+
+        if (Array.isArray(movie.actors)) {
+            for (const ref of movie.actors) {
+                const actor = await actorCatalogue.getActor(ref.actorId);
+                if (!actor) {
+                    continue;
+                }
+
+                actorsResolved.push({
+                    id: actor._id.toString(),
+                    name: actor.name,
+                    portrait: actor.portrait,
+                    description: actor.description,
+                    role: ref.role
+                });
+            }
+        }
+
+        res.render('movieDetails', {
+            ...movie,
+            id: movie._id.toString(),
+            genresText: movie.genre?.join(', '),
+            countriesText: movie.countryOfProduction?.join(', '),
+            hasActors: actorsResolved.length > 0,
+            actors: actorsResolved
+        });
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
 });
 
-router.get('/personDetails', (req, res) => {
+router.get('/personDetails/:id', async (req, res) => {
+    try {
+        const actorId = new ObjectId(req.params.id);
+        const actor = await actorCatalogue.getActor(actorId);
+    
+        if (!actor) {
+            return res.status(404).send('Actor not found');
+        }
 
-    res.render('personDetails', {
+        const birthDate = new Date(actor.dateOfBirth);
+        const birthdayFormatted = birthDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        });
 
-    });
-
+        const today = new Date();
+        const age = today.getFullYear() - birthDate.getFullYear();
+    
+        res.render('personDetails', {
+            ...actor,
+            birthdayFormatted,
+            age
+        });
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
 });
+
 
 router.get('/addNewMovie', (req, res) => {
 
     res.render('addNewMovie', {
 
+    });
+
+});
+
+router.post('/addNewMovie', (req, res) => {
+
+    res.render('addNewMovie', {
+        title: req.body.title
     });
 
 });
